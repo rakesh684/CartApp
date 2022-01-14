e9cf736e78ce898db0d58b9c194b1b7e9e5482f8
Rakesh kumar <dr.rakesh684@gmail.com>
2022-01-14 12:43:03 +0530

    adding cartItem list

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,31 +1,61 @@
-import React from "react";
+import React from 'react';
+// import { FiMinusCircle } from "react-icons/fi";
 class CartItem extends React.Component {
-  render() {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
 
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+  render () {
+    const { price, title, qty } = this.state;
     return (
-      <div className="cart-itam">
-
+      <div className="cart-item">
         <div className="left-block">
-            <img style={styles.image}/>
+          <img alt="" style={styles.image} />
         </div>
-
         <div className="right-block">
-            <div style={ {fontSize: 25}}>Phone</div>
-            <div style={ {color: '#777'}}>Rs 999</div>
-            <div style={ {color: '#777'}}>Qty: 1</div>
-            <div className="cart-item-actions">
-                 {/*buttons */}
-                <img alt="increase" className="action-icons" src="AiFillPlusCircle" />
-                <img alt="decrease" className="action-icons" src="AiOutlineMinusCircle" />
-                <img alt="delete" className="action-icons" src="AiOutlineDelete" />
-            </div>
-
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+           
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
+            <button className="action-icons" onClick={this.increaseQuantity} >
+            decrease  
+           
+            </button>
+          </div>
         </div>
-
-       </div>
+      </div>
     );
   }
 }
+
 const styles = {
   image: {
     height: 110,
